81a9698b2f2817946872642fbc512524099745f7
Saivarunt <saivarunthirunavukarasu@gmail.com>
2023-09-02 13:31:32 +0530

    Fibonacci with recursion

--- fibo.java
+++ fibo.java
@@ -1,25 +1,55 @@
 // Fibonacci Series
 import java.util.*;
+//public class fibo{
+//	public static void main(String args[]){
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter a number: ");
+//		int num = sc.nextInt();
+//		int i=0;
+//		int a=0;
+//		int b=1;
+//		int temp;
+//		System.out.println("Fibonacci Series: ");
+//		while(i+2<num){
+//			if (i==0){
+//			System.out.println(a);
+//			System.out.println(b);
+//			}
+//			temp=b;
+//			b= a+b;
+//			a=temp;
+//			System.out.println(b);
+//			i++;
+//		}
+//	}
+//}
+
+class Fb{
+	int fb(int a1, int b1,int number){
+		if(number==0){
+			System.out.println(a1+b1);
+			return 0;
+		}
+		else{
+		int temp=b1;
+		b1=b1+a1;
+		a1=temp;
+		System.out.println(b1);
+		fb(a1,b1,number-1);
+		}
+	return 0;
+	}}
 public class fibo{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number: ");
 		int num = sc.nextInt();
-		int i=0;
 		int a=0;
 		int b=1;
-		int temp;
-		System.out.println("Fibonacci Series: ");
-		while(i+2<num){
-			if (i==0){
-			System.out.println(a);
-			System.out.println(b);
-			}
-			temp=b;
-			b= a+b;
-			a=temp;
-			System.out.println(b);
-			i++;
-		}
+		System.out.println(a);
+		System.out.println(b);
+		Fb obj=new Fb();
+		obj.fb(a,b,num);
+
 	}
 }
