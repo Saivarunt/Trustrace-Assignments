7ebb8748c4212f471333b3dc2d0bfeb25dbab329
Saivarunt <saivarunthirunavukarasu@gmail.com>
2023-09-04 14:31:24 +0530

    Protected member access problem

---
+++ pk1/Ptdeg.java
@@ -0,0 +1,6 @@
+package pk1;
+public class Ptdeg{
+	protected void sample(){
+		System.out.println("pkg1");
+	}
+}
---
+++ pk2/Access.java
@@ -0,0 +1,10 @@
+
+import pk1.*;
+public class Access{
+	public static void main(String args[]){
+		System.out.println("pkg2");
+		Ptdeg obj = new Ptdeg();
+		// obj.sample();
+		// cannot access since they arent part of the same package
+	}
+}
