f060b3427aaec87a40b66a5ef63d5fade7cc5c75
Saivarunt <saivarunthirunavukarasu@gmail.com>
2023-09-28 15:16:06 +0530

    Updated architecture

--- dbms/sqlqueries/Sports DB.sql
+++ dbms/sqlqueries/Sports DB.sql
@@ -1,40 +1,41 @@
--- create database Sport;
+create database Sport;
 use Sport;
 
--- create table team(
--- team_id varchar(10) primary key,
--- name text,
--- owner varchar(50)
--- );
+create table team(
+team_id varchar(10) primary key,
+name text,
+owner varchar(50)
+);
 
--- create table Player(
--- player_id int primary key auto_increment,
--- name varchar(50),
--- dob date,
--- no_of_match_played int,
--- team_id varchar(10),
--- foreign key(team_id) references team(team_id)
--- );
+create table Player(
+player_id int primary key auto_increment,
+name varchar(50),
+dob date,
+no_of_match_played int,
+team_id varchar(10),
+foreign key(team_id) references team(team_id)
+);
 
--- create table Matches (
--- match_id int primary key auto_increment,
--- place varchar(50),
--- date_of_match date,
--- goals int,
--- own_score int,
--- opp_score int,
--- home varchar(10),
--- away varchar(10),
--- foreign key(home) references team(team_id),
--- foreign key(away) references team(team_id)
--- );
+create table Matches (
+match_id int primary key auto_increment,
+place varchar(50),
+season int,
+date_of_match date,
+goals int,
+own_score int,
+opp_score int,
+home varchar(10),
+away varchar(10),
+foreign key(home) references team(team_id),
+foreign key(away) references team(team_id)
+);
 
--- create table MatchPlayed (
--- minutes time,
--- assist int,
--- goals_scored int,
--- player_id int,
--- match_id int,
--- foreign key(player_id) references Player(player_id),
--- foreign key(match_id) references Matches(match_id)
--- );
+create table MatchPlayed (
+minutes time,
+assist int,
+goals_scored int,
+player_id int,
+match_id int,
+foreign key(player_id) references Player(player_id),
+foreign key(match_id) references Matches(match_id)
+);
